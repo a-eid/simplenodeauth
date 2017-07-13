5f43dd13013f67c511bc69063b3d0ca259c0383a
Ahmed Eid <a.eid@yandex.com>
2017-07-13 11:04:28 +0300

    added auth and middleware

--- src/controller.js
+++ src/controller.js
@@ -1,20 +1,19 @@
+import bcrypt from 'bcryptjs'
+
 import User from './userModel'
 import { getMongooseErrors } from './helper'
+var salt = bcrypt.genSaltSync(10);
 
 export const home = (req, res) => {
   res.render('index')
 }
 
 export const register = (req, res) => {
-  if (notLoggedin(req))
-    return res.render('register')
-  res.redirect('/secret')
+   res.render('register')
 }
 
 export const login = (req, res) => {
-  if (notLoggedin(req))
-    return res.render('login')
-  res.redirect('/secret')
+   res.render('login')
 }
 
 export const logout = (req, res) => {
@@ -23,16 +22,12 @@ export const logout = (req, res) => {
 }
 
 export const secret = (req, res) => {
-  if (notLoggedin(req))
-    return res.redirect('/login')
-  res.render('secret' , {
-    user : req.session.user 
-  })
+   res.render('secret')
 }
 
 export const create = async (req, res) => {
   try {
-    let user = new User(req.body)
+    let user = new User(Object.assign(req.body, { password: bcrypt.hashSync(req.body.password) }))
     await user.save()
     req.session.user = JSON.stringify(user)
     req.loggedin = true
@@ -49,14 +44,30 @@ export const create = async (req, res) => {
 export const authenticate = async (req, res) => {
   try {
     let user = await User.findOne({ email: req.body.email })
-    if (!user || user.password !== req.body.password)
+    if (!user || !bcrypt.compareSync(req.body.password, user.password))
       return res.render('login', ["invalid credintials"])
     req.session.user = user
     res.redirect('/secret')
   } catch (e) {
+    console.log(e)
     res.send("ERROR: SERVER ERROR")
   }
 }
 
-const isLoggedin = req => req.session && req.session.user
-const notLoggedin = req => !req.session || !req.session.user 
+export const loggedinRequired = (req, res, next) => {
+  if (req.session && req.session.user){
+    res.locals.user = req.session.user
+    next() 
+  }else{
+    res.redirect('/login')
+  }
+}
+
+export const loggedinForbidden = (req , res, next) => {
+  if (!req.session || !req.session.user) {
+    req.session && req.session.destroy()
+    next()
+  }else{
+   res.redirect('/secret')
+  }
+}
--- src/router.js
+++ src/router.js
@@ -2,16 +2,16 @@ import express from 'express'
 const Router = express.Router()
 import {
   home, register, login, logout, secret,
-  create, authenticate
+  create, authenticate , loggedinForbidden , loggedinRequired
 } from './controller'
 
 
 // serving pages 
 Router.get('/', home)
-Router.get('/register', register)
-Router.get('/login', login)
-Router.get('/secret', secret)
-Router.get('/logout', logout)
+Router.get('/register', loggedinForbidden  , register)
+Router.get('/login', loggedinForbidden ,login)
+Router.get('/secret', loggedinRequired, secret)
+Router.get('/logout' ,logout)
 
 Router.post('/register', create)
 Router.post('/login', authenticate)
